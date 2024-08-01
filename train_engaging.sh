45b34b2e793d91fa0779efb782bd9c68a4fcfb01
ahnobari <ahnobari@eofe10.mit.edu>
2024-08-01 15:23:10 -0400

    small changes

--- train_engaging.sh
+++ train_engaging.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH -n 100
+#SBATCH -n 200
 #SBATCH -p pi_faez
 #SBATCH --gres=gpu:4
-#SBATCH --mem=1000000
+#SBATCH --mem=2000000
 #SBATCH -t 4-00:00
 
 #SBATCH -o log.out
@@ -13,4 +13,4 @@ module load cuda/12.4.0-x86_64
 export PATH=/home/ahnobari/miniforge3/bin:${PATH}
 source activate topology
 
-python train.py --multi_gpu --mixed_precision --Field_hidden_size 20000 --Field_n_layers 8 --BC_emb_size 128 --C_mapping_size 512 --samples 512 --batch_size 16
+python train.py --multi_gpu --mixed_precision --Field_hidden_size 25000 --Field_n_layers 8 --BC_emb_size 128 --C_mapping_size 512 --samples 512 --batch_size 16
