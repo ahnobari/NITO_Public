68bc3a200f0f01371a495b05b9266e8e5df9ce5e
Amin <amin@decodeelectric.mit.edu>
2024-08-23 08:08:58 -0400

    3D animation code added

---
+++ ANI3D/__init__.py
@@ -0,0 +1,443 @@
+'''
+Visualization tools for 3D
+'''
+
+import pyvista as pv
+import numpy as np
+import mcubes
+from .geometry import generate_structured_mesh, get_pv
+import moviepy.editor as mp
+import os
+import uuid
+from tqdm import trange
+from scipy.spatial import KDTree
+import copy
+
+def animate_cross_voxelized(top, shape, bc=None, load=None, n_frames = 50, fps=25, f_name='top_anim.mp4', grid=False, face_color='white', edge_color='#87CEFA', highlight_color='royalblue', meat_color='indigo'):
+    elements, nodes = generate_structured_mesh(shape,shape)
+
+    f_name_t = uuid.uuid4().hex
+
+    os.makedirs(f_name_t, exist_ok=True)
+
+    max_x = nodes[elements[top[:,0]]].max(1)[:,0].max()
+    min_x = nodes[elements[top[:,0]]].min(1)[:,0].min()
+
+    if bc is not None:
+        x_points = bc[bc[:,3]==1,0:3]*shape.max()
+        y_points = bc[bc[:,4]==1,0:3]*shape.max()
+        z_points = bc[bc[:,5]==1,0:3]*shape.max()
+
+        x_mesh = pv.PolyData(x_points)
+        y_mesh = pv.PolyData(y_points)
+        z_mesh = pv.PolyData(z_points)
+    
+    if load is not None:
+        load_points = load[:,0:3]*shape.max()
+
+
+    range_of_clip = np.linspace(max_x-0.001,min_x,n_frames)
+    mesh = get_pv(nodes, elements[top[:,0]])
+
+    def get_frame(i):
+        plotter = pv.Plotter(lighting='three lights')
+        plotter.clear()
+        plotter.enable_anti_aliasing('msaa')
+        c_mesh = mesh.clip(normal=(1,0,0),origin=(range_of_clip[i],0,0),invert=True)
+        c_face = c_mesh.clip(normal=(1,0,0),origin=(range_of_clip[i]-0.001,0,0),invert=False)
+        c_edges = c_face.extract_feature_edges(0,
+                                               boundary_edges=True,
+                                               feature_edges=False,
+                                               manifold_edges=False,
+                                               non_manifold_edges=False)
+
+        plotter.add_mesh(mesh, show_edges=False, edge_color='royalblue', color='white', opacity=0.0)
+        plotter.add_mesh(c_mesh, show_edges=True, edge_color=edge_color, color=face_color, opacity=1.0)
+        plotter.add_mesh(c_edges, color=highlight_color, line_width=5)
+
+
+        if bc is not None:
+            plotter.add_mesh(x_mesh, color='red', point_size=10, render_points_as_spheres=True, opacity=0.7)
+            plotter.add_mesh(y_mesh, color='green', point_size=10, render_points_as_spheres=True, opacity=0.7)
+            plotter.add_mesh(z_mesh, color='blue', point_size=10, render_points_as_spheres=True, opacity=0.7)
+
+        if load is not None:
+            for i in range(load_points.shape[0]):
+                plotter.add_mesh(pv.Arrow(load_points[i], load[i,3:6].astype(float), scale=6, shaft_radius=0.02, tip_radius=0.06, tip_length=0.2), color='indigo')
+        
+        if grid:
+            plotter.show_grid()
+
+        return plotter
+
+    for i in trange(range_of_clip.shape[0]):
+        p = get_frame(i)
+        p.screenshot(f_name_t + f'/{i}.png')
+        p.close()
+    
+    # make video
+    clip = mp.ImageSequenceClip([f_name_t + f'/{i}.png' for i in range(range_of_clip.shape[0])], fps=fps)
+    clip = mp.concatenate_videoclips([clip, clip.fx(mp.vfx.time_mirror)])
+    clip.write_videofile(f_name)
+
+    # remove temp files
+    for i in range(range_of_clip.shape[0]):
+        os.remove(f_name_t + f'/{i}.png')
+    os.removedirs(f_name_t)
+
+def animate_cross_smooth(top, shape, bc=None, load=None, n_frames = 50, fps=25, f_name='top_anim.mp4', grid=False, face_color='lightgrey', edge_color=None, highlight_color='black', meat_color='indigo'):
+    f_name_t = uuid.uuid4().hex
+
+    os.makedirs(f_name_t, exist_ok=True)
+
+    vertices, triangles = mcubes.marching_cubes(np.pad(top.reshape(shape),[[1,1]]*3), 0.5)
+    vertices = vertices - 0.5
+    mesh = pv.make_tri_mesh(vertices, triangles)
+    mesh = mesh.smooth(n_iter=1000, relaxation_factor=0.01)
+
+    max_x = mesh.points[:,0].max()
+    min_x = mesh.points[:,0].min()
+
+    range_of_clip = np.linspace(max_x-0.001,min_x+0.001,n_frames)
+
+
+    if bc is not None or load is not None:
+        tree = KDTree(mesh.points)
+    if bc is not None:
+        x_points_ = bc[bc[:,3]==1,0:3]*shape.max()
+        y_points_ = bc[bc[:,4]==1,0:3]*shape.max()
+        z_points_ = bc[bc[:,5]==1,0:3]*shape.max()
+    
+
+    
+        x_points = mesh.points[tree.query(x_points_)[1]]
+        y_points = mesh.points[tree.query(y_points_)[1]]
+        z_points = mesh.points[tree.query(z_points_)[1]]
+        
+        del_x = np.linalg.norm(x_points-x_points_, axis=1)
+        del_y = np.linalg.norm(y_points-y_points_, axis=1)
+        del_z = np.linalg.norm(z_points-z_points_, axis=1)
+
+        x_points[del_x>2] = x_points_[del_x>2]
+        y_points[del_y>2] = y_points_[del_y>2]
+        z_points[del_z>2] = z_points_[del_z>2]
+
+        x_mesh = pv.PolyData(x_points)
+        y_mesh = pv.PolyData(y_points)
+        z_mesh = pv.PolyData(z_points)
+    
+    if load is not None:
+        load_points = load[:,0:3]*shape.max()
+        load_points = mesh.points[tree.query(load_points)[1]]
+
+    if edge_color is None:
+        show_edges = False
+    else:
+        show_edges = True
+
+    def get_frame(i):
+        c_mesh = mesh.clip(normal=(1,0,0),origin=(range_of_clip[i],0,0),invert=True)
+        c_face = c_mesh.clip(normal=(1,0,0),origin=(range_of_clip[i]-0.001,0,0),invert=False)
+        c_edges = c_face.extract_feature_edges(0,
+                                               boundary_edges=True,
+                                               feature_edges=False,
+                                               manifold_edges=False,
+                                               non_manifold_edges=False)
+
+        #make a plane surface of the cut location
+        plane_mesh = pv.Plane(center=(range_of_clip[i],shape[1]/2,shape[2]/2), direction=(1,0,0), i_size=shape.max()*2, j_size=shape.max()*2).triangulate().subdivide(3)
+        cut_surface = plane_mesh.clip_surface(mesh)
+
+        
+        plotter = pv.Plotter(lighting='three lights')
+        plotter.enable_anti_aliasing('msaa')
+        # plotter.enable_eye_dome_lighting()
+        plotter.add_mesh(mesh, show_edges=False, edge_color='royalblue', color='#000000', opacity=0.0)
+        plotter.add_mesh(c_mesh, show_edges=show_edges, edge_color=edge_color, color=face_color, opacity=1.0, smooth_shading=True)
+        if cut_surface.n_points > 0:
+            plotter.add_mesh(cut_surface, color=meat_color)
+        plotter.add_mesh(c_edges, color=highlight_color, line_width=3)
+
+        if bc is not None:
+            plotter.add_mesh(x_mesh, color='red', point_size=10, render_points_as_spheres=True, opacity=0.7)
+            plotter.add_mesh(y_mesh, color='green', point_size=10, render_points_as_spheres=True, opacity=0.7)
+            plotter.add_mesh(z_mesh, color='blue', point_size=10, render_points_as_spheres=True, opacity=0.7)
+
+        if load is not None:
+            for i in range(load_points.shape[0]):
+                plotter.add_mesh(pv.Arrow(load_points[i], load[i,3:6].astype(float), scale=6, shaft_radius=0.02, tip_radius=0.06, tip_length=0.2), color='indigo')
+        
+        if grid:
+            plotter.show_grid()
+
+        return plotter
+
+    for i in trange(range_of_clip.shape[0]):
+        plotter = get_frame(i)
+        plotter.screenshot(f_name_t + f'/{i}.png')
+    
+    plotter.close()
+
+    clip = mp.ImageSequenceClip([f_name_t + f'/{i}.png' for i in range(n_frames)], fps=fps)
+    clip = mp.concatenate_videoclips([clip, clip.fx(mp.vfx.time_mirror)])
+
+    clip.write_videofile(f_name)
+
+    for i in range(n_frames):
+        os.remove(f_name_t + f'/{i}.png')
+    
+    os.rmdir(f_name_t)
+
+def animate_rotation_voxelized(top, shape, bc=None, load=None, n_frames = 50, fps=25, f_name='top_anim.mp4', grid=False, face_color='white', edge_color='#87CEFA'):
+    elements, nodes = generate_structured_mesh(shape,shape)
+
+    f_name_t = uuid.uuid4().hex
+
+    os.makedirs(f_name_t, exist_ok=True)
+
+    if bc is not None:
+        x_points = bc[bc[:,3]==1,0:3]*shape.max()
+        y_points = bc[bc[:,4]==1,0:3]*shape.max()
+        z_points = bc[bc[:,5]==1,0:3]*shape.max()
+
+        x_mesh = pv.PolyData(x_points)
+        y_mesh = pv.PolyData(y_points)
+        z_mesh = pv.PolyData(z_points)
+    
+    if load is not None:
+        load_points = load[:,0:3]*shape.max()
+
+    mesh = get_pv(nodes, elements[top[:,0]])
+
+    plotter = pv.Plotter(lighting='three lights')
+    plotter.clear()
+    plotter.enable_anti_aliasing('msaa')
+    plotter.add_mesh(mesh, show_edges=False, edge_color='royalblue', color='white', opacity=1.0)
+    if bc is not None:
+        plotter.add_mesh(x_mesh, color='red', point_size=10, render_points_as_spheres=True, opacity=0.7)
+        plotter.add_mesh(y_mesh, color='green', point_size=10, render_points_as_spheres=True, opacity=0.7)
+        plotter.add_mesh(z_mesh, color='blue', point_size=10, render_points_as_spheres=True, opacity=0.7)
+
+    if load is not None:
+        for i in range(load_points.shape[0]):
+            plotter.add_mesh(pv.Arrow(load_points[i], load[i,3:6].astype(float), scale=6, shaft_radius=0.02, tip_radius=0.06, tip_length=0.2), color='indigo')
+    
+    if grid:
+        plotter.show_grid()
+    plotter.screenshot(f_name_t + f'/temp.png')
+    os.remove(f_name_t + f'/temp.png')
+    c_p = copy.deepcopy(plotter.camera_position)
+    plotter.close()
+
+    def get_frame(i):
+        plotter = pv.Plotter(lighting='three lights')
+        plotter.clear()
+        plotter.enable_anti_aliasing('msaa')
+        plotter.add_mesh(mesh, show_edges=True, edge_color=edge_color, color=face_color, opacity=1.0)
+        plotter.camera_position = c_p
+        plotter.camera.azimuth += i*360/n_frames
+
+        if bc is not None:
+            plotter.add_mesh(x_mesh, color='red', point_size=10, render_points_as_spheres=True, opacity=0.7)
+            plotter.add_mesh(y_mesh, color='green', point_size=10, render_points_as_spheres=True, opacity=0.7)
+            plotter.add_mesh(z_mesh, color='blue', point_size=10, render_points_as_spheres=True, opacity=0.7)
+
+        if load is not None:
+            for i in range(load_points.shape[0]):
+                plotter.add_mesh(pv.Arrow(load_points[i], load[i,3:6].astype(float), scale=6, shaft_radius=0.02, tip_radius=0.06, tip_length=0.2), color='indigo')
+        
+        if grid:
+            plotter.show_grid()
+
+        return plotter
+
+    for i in trange(n_frames):
+        p = get_frame(i)
+        p.screenshot(f_name_t + f'/{i}.png')
+        p.close()
+    
+    # make video
+    clip = mp.ImageSequenceClip([f_name_t + f'/{i}.png' for i in range(n_frames)], fps=fps)
+    clip.write_videofile(f_name)
+
+    # remove temp files
+    for i in range(n_frames):
+        os.remove(f_name_t + f'/{i}.png')
+    os.removedirs(f_name_t)
+
+def animate_rotation_smooth(top, shape, bc=None, load=None, n_frames = 50, fps=25, f_name='top_anim.mp4', grid=False, face_color='lightgrey', edge_color=None):
+    f_name_t = uuid.uuid4().hex
+
+    os.makedirs(f_name_t, exist_ok=True)
+
+    vertices, triangles = mcubes.marching_cubes(np.pad(top.reshape(shape),[[1,1]]*3), 0.5)
+    vertices = vertices - 0.5
+    mesh = pv.make_tri_mesh(vertices, triangles)
+    mesh = mesh.smooth(n_iter=1000, relaxation_factor=0.01)
+
+    if bc is not None or load is not None:
+        tree = KDTree(mesh.points)
+    if bc is not None:
+        x_points_ = bc[bc[:,3]==1,0:3]*shape.max()
+        y_points_ = bc[bc[:,4]==1,0:3]*shape.max()
+        z_points_ = bc[bc[:,5]==1,0:3]*shape.max()
+    
+        x_points = mesh.points[tree.query(x_points_)[1]]
+        y_points = mesh.points[tree.query(y_points_)[1]]
+        z_points = mesh.points[tree.query(z_points_)[1]]
+        
+        del_x = np.linalg.norm(x_points-x_points_, axis=1)
+        del_y = np.linalg.norm(y_points-y_points_, axis=1)
+        del_z = np.linalg.norm(z_points-z_points_, axis=1)
+
+        x_points[del_x>2] = x_points_[del_x>2]
+        y_points[del_y>2] = y_points_[del_y>2]
+        z_points[del_z>2] = z_points_[del_z>2]
+
+        x_mesh = pv.PolyData(x_points)
+        y_mesh = pv.PolyData(y_points)
+        z_mesh = pv.PolyData(z_points)
+    
+    if load is not None:
+        load_points = load[:,0:3]*shape.max()
+        load_points = mesh.points[tree.query(load_points)[1]]
+
+    if edge_color is None:
+        show_edges = False
+    else:
+        show_edges = True
+
+    plotter = pv.Plotter(lighting='three lights')
+    plotter.enable_anti_aliasing('msaa')
+    plotter.add_mesh(mesh, show_edges=False, edge_color='royalblue', color='white', opacity=1.0, smooth_shading=True)
+    if bc is not None:
+        plotter.add_mesh(x_mesh, color='red', point_size=10, render_points_as_spheres=True, opacity=0.7)
+        plotter.add_mesh(y_mesh, color='green', point_size=10, render_points_as_spheres=True, opacity=0.7)
+        plotter.add_mesh(z_mesh, color='blue', point_size=10, render_points_as_spheres=True, opacity=0.7)
+    
+    if load is not None:
+        for i in range(load_points.shape[0]):
+            plotter.add_mesh(pv.Arrow(load_points[i], load[i,3:6].astype(float), scale=6, shaft_radius=0.02, tip_radius=0.06, tip_length=0.2), color='indigo')
+    
+    if grid:
+        plotter.show_grid()
+    plotter.screenshot(f_name_t + f'/temp.png')
+    os.remove(f_name_t + f'/temp.png')
+    c_p = copy.deepcopy(plotter.camera_position)
+    plotter.close()
+
+    def get_frame(i):
+        plotter = pv.Plotter(lighting='three lights')
+        plotter.enable_anti_aliasing('msaa')
+        plotter.add_mesh(mesh, show_edges=show_edges, edge_color=edge_color, color=face_color, opacity=1.0, smooth_shading=True)
+        plotter.camera_position = c_p
+        plotter.camera.azimuth += i*360/n_frames
+
+        if bc is not None:
+            plotter.add_mesh(x_mesh, color='red', point_size=10, render_points_as_spheres=True, opacity=0.7)
+            plotter.add_mesh(y_mesh, color='green', point_size=10, render_points_as_spheres=True, opacity=0.7)
+            plotter.add_mesh(z_mesh, color='blue', point_size=10, render_points_as_spheres=True, opacity=0.7)
+
+        if load is not None:
+            for i in range(load_points.shape[0]):
+                plotter.add_mesh(pv.Arrow(load_points[i], load[i,3:6].astype(float), scale=6, shaft_radius=0.02, tip_radius=0.06, tip_length=0.2), color='indigo')
+        
+        if grid:
+            plotter.show_grid()
+
+        return plotter
+
+    for i in trange(n_frames):
+        p = get_frame(i)
+        p.screenshot(f_name_t + f'/{i}.png')
+        p.close()
+    
+    # make video
+    clip = mp.ImageSequenceClip([f_name_t + f'/{i}.png' for i in range(n_frames)], fps=fps)
+    clip.write_videofile(f_name)
+
+    # remove temp files
+    for i in range(n_frames):
+        os.remove(f_name_t + f'/{i}.png')
+    os.removedirs(f_name_t)
+
+def animate_top_series(tops, shape, bc=None, load=None, fps=25, f_name='top_anim.mp4', grid=False, face_color='lightgrey', edge_color=None):
+    f_name_t = uuid.uuid4().hex
+
+    os.makedirs(f_name_t, exist_ok=True)
+
+    vertices, triangles = mcubes.marching_cubes(np.pad(tops[-1].reshape(shape),[[1,1]]*3), 0.5)
+    vertices = vertices - 0.5
+    mesh = pv.make_tri_mesh(vertices, triangles)
+    mesh = mesh.smooth(n_iter=1000, relaxation_factor=0.01)
+
+    if bc is not None or load is not None:
+        tree = KDTree(mesh.points)
+    if bc is not None:
+        x_points_ = bc[bc[:,3]==1,0:3]*shape.max()
+        y_points_ = bc[bc[:,4]==1,0:3]*shape.max()
+        z_points_ = bc[bc[:,5]==1,0:3]*shape.max()
+    
+        x_points = mesh.points[tree.query(x_points_)[1]]
+        y_points = mesh.points[tree.query(y_points_)[1]]
+        z_points = mesh.points[tree.query(z_points_)[1]]
+        
+        del_x = np.linalg.norm(x_points-x_points_, axis=1)
+        del_y = np.linalg.norm(y_points-y_points_, axis=1)
+        del_z = np.linalg.norm(z_points-z_points_, axis=1)
+
+        x_points[del_x>2] = x_points_[del_x>2]
+        y_points[del_y>2] = y_points_[del_y>2]
+        z_points[del_z>2] = z_points_[del_z>2]
+
+        x_mesh = pv.PolyData(x_points)
+        y_mesh = pv.PolyData(y_points)
+        z_mesh = pv.PolyData(z_points)
+    
+    if load is not None:
+        load_points = load[:,0:3]*shape.max()
+        load_points = mesh.points[tree.query(load_points)[1]]
+
+    if edge_color is None:
+        show_edges = False
+    else:
+        show_edges = True
+    
+    def get_frame(i):
+        vertices, triangles = mcubes.marching_cubes(np.pad(tops[i].reshape(shape),[[1,1]]*3), 0.5)
+        vertices = vertices - 0.5
+        mesh_ = pv.make_tri_mesh(vertices, triangles)
+        mesh_ = mesh_.smooth(n_iter=1000, relaxation_factor=0.01)
+
+        plotter = pv.Plotter(lighting='three lights')
+        plotter.enable_anti_aliasing('msaa')
+        plotter.add_mesh(mesh, show_edges=show_edges, edge_color=edge_color, color=face_color, opacity=0.0)
+        if mesh_.n_points > 0:
+            plotter.add_mesh(mesh_, show_edges=show_edges, edge_color=edge_color, color=face_color, opacity=1.0, smooth_shading=True)
+        if bc is not None:
+            plotter.add_mesh(x_mesh, color='red', point_size=10, render_points_as_spheres=True, opacity=0.7)
+            plotter.add_mesh(y_mesh, color='green', point_size=10, render_points_as_spheres=True, opacity=0.7)
+            plotter.add_mesh(z_mesh, color='blue', point_size=10, render_points_as_spheres=True, opacity=0.7)
+        
+        if load is not None:
+            for i in range(load_points.shape[0]):
+                plotter.add_mesh(pv.Arrow(load_points[i], load[i,3:6].astype(float), scale=6, shaft_radius=0.02, tip_radius=0.06, tip_length=0.2), color='indigo')
+        
+        if grid:
+            plotter.show_grid()
+
+        return plotter
+    
+    for i in trange(len(tops)):
+        p = get_frame(i)
+        p.screenshot(f_name_t + f'/{i}.png')
+        p.close()
+
+    # make video
+    clip = mp.ImageSequenceClip([f_name_t + f'/{i}.png' for i in range(len(tops))], fps=fps)
+    clip.write_videofile(f_name)
+
+    # remove temp files
+    for i in range(len(tops)):
+        os.remove(f_name_t + f'/{i}.png')
+    os.removedirs(f_name_t)
---
+++ ANI3D/geometry.py
@@ -0,0 +1,195 @@
+import numpy as np
+import pyvista as pv
+
+def get_pv(node_positions, elements):
+    faces = []
+    for e in elements:
+        if len(e) == 4:
+            faces.append([4, e[0], e[1], e[2], e[2]])
+            faces.append([4, e[0], e[1], e[3], e[3]])
+            faces.append([4, e[0], e[2], e[3], e[3]])
+            faces.append([4, e[1], e[2], e[3], e[3]])
+        else:
+            faces.append([4, e[0], e[1], e[2], e[3]])
+            faces.append([4, e[4], e[5], e[6], e[7]])
+            faces.append([4, e[0], e[1], e[5], e[4]])
+            faces.append([4, e[1], e[2], e[6], e[5]])
+            faces.append([4, e[2], e[3], e[7], e[6]])
+            faces.append([4, e[3], e[0], e[4], e[7]])
+
+    faces = np.hstack(faces)
+
+    pv_mesh = pv.PolyData(node_positions, faces)
+
+    return pv_mesh
+
+
+def generate_structured_mesh(dim, nel):
+    """
+    This function generates a structured mesh for a given dimension and number of elements.
+
+    Parameters:
+        dim (np.array): Array with the dimension of the domain. Either (2,) or (3,)
+        nel (np.array): Array with the number of elements in each direction. Either (2,) or (3,)
+
+    Returns:
+        node_positions (np.array): Array with the nodal positions. Shape (n_nodes, dim)
+        elements (np.array): Array with the elements. Shape (nel**dim, 4 or 8) for 2D or 3D respectively (Quad and Hex Elements).
+    """
+
+    if len(dim) != len(nel):
+        raise Exception("Dimensions of dim and nel do not match")
+
+    if len(dim) == 2:
+
+        nx, ny = nel[0] + 1, nel[1] + 1
+        num_elem = nel[0] * nel[1]
+
+        L, H = dim[0], dim[1]
+
+        # Create Structured Mesh
+        node_positions = np.concatenate(
+            np.expand_dims(
+                np.meshgrid(np.linspace(0, L, nx), np.linspace(0, H, ny)), -1
+            ),
+            -1,
+        ).reshape(-1, 2)
+
+        elements = np.zeros([num_elem, 4], dtype=int)
+
+        counter = 0
+        for i in range(nx - 1):
+            for j in range(ny - 1):
+                elements[counter] = [
+                    j * nx + i,
+                    j * nx + i + 1,
+                    (j + 1) * nx + i + 1,
+                    (j + 1) * nx + i,
+                ]
+                counter += 1
+    elif len(dim) == 3:
+        nx, ny, nz = nel[0] + 1, nel[1] + 1, nel[2] + 1
+        num_elem = nel[0] * nel[1] * nel[2]
+
+        L, H, W = dim[0], dim[1], dim[2]
+
+        # Create Structured Mesh
+        node_positions = np.concatenate(
+            np.expand_dims(
+                np.meshgrid(
+                    np.linspace(0, L, nx), np.linspace(0, H, ny), np.linspace(0, W, nz)
+                ),
+                -1,
+            ),
+            -1,
+        ).reshape(-1, 3)
+
+        elements = np.zeros([num_elem, 8], dtype=int)
+
+        counter = 0
+        for i in range(nx - 1):
+            for j in range(ny - 1):
+                for k in range(nz - 1):
+                    # [k*nx*ny+j*nx+i,k*nx*ny+j*nx+i+1,k*nx*ny+(j+1)*nx+i+1,k*nx*ny+(j+1)*nx+i,
+                    #                     (k+1)*nx*ny+j*nx+i,(k+1)*nx*ny+j*nx+i+1,(k+1)*nx*ny+(j+1)*nx+i+1,(k+1)*nx*ny+(j+1)*nx+i]
+                    elements[counter] = [
+                        nz * i + nz * nx * j + k,
+                        nz * (i + 1) + nz * nx * j + k,
+                        nz * (i + 1) + nz * nx * (j + 1) + k,
+                        nz * i + nz * nx * (j + 1) + k,
+                        nz * i + nz * nx * j + k + 1,
+                        nz * (i + 1) + nz * nx * j + k + 1,
+                        nz * (i + 1) + nz * nx * (j + 1) + k + 1,
+                        nz * i + nz * nx * (j + 1) + k + 1,
+                    ]
+                    counter += 1
+    else:
+        raise Exception("Only 2D and 3D meshes are supported")
+
+    return elements, node_positions
+
+
+triangle_args = ["-setnumber", "Mesh.SaveWithoutOrphans", "1"]
+quad_args = [
+    "-setnumber",
+    "Mesh.Algorithm",
+    "6",
+    "-setnumber",
+    "Mesh.RecombinationAlgorithm",
+    "3",
+    "-setnumber",
+    "Mesh.RecombineAll",
+    "1",
+    "-setnumber",
+    "Mesh.SaveWithoutOrphans",
+    "1",
+]
+tet_args = ["-setnumber", "Mesh.SaveWithoutOrphans", "1"]
+hex_args = [
+    "-setnumber",
+    "Mesh.Algorithm",
+    "8",
+    "-setnumber",
+    "Mesh.RecombinationAlgorithm",
+    "3",
+    "-setnumber",
+    "Mesh.RecombineAll",
+    "1",
+    "-setnumber",
+    "Mesh.SubdivisionAlgorithm",
+    "2",
+    "-setnumber",
+    "Mesh.Algorithm3D",
+    "1",
+    "-setnumber",
+    "Mesh.Recombine3DAll",
+    "1",
+    "-setnumber",
+    "Mesh.SaveWithoutOrphans",
+    "1",
+    "-setnumber",
+    "Mesh.Smoothing",
+    "5",
+]
+
+tri_quad_args = [
+    "-setnumber",
+    "Mesh.Algorithm",
+    "6",
+    "-setnumber",
+    "Mesh.RecombinationAlgorithm",
+    "5",
+    "-setnumber",
+    "Mesh.RecombineAll",
+    "1",
+    "-setnumber",
+    "Mesh.SaveWithoutOrphans",
+    "1",
+]
+
+tet_hex_args = [
+    "-setnumber",
+    "Mesh.Algorithm",
+    "8",
+    "-setnumber",
+    "Mesh.RecombinationAlgorithm",
+    "5",
+    "-setnumber",
+    "Mesh.RecombineAll",
+    "1",
+    "-setnumber",
+    "Mesh.SubdivisionAlgorithm",
+    "2",
+    "-setnumber",
+    "Mesh.Algorithm3D",
+    "1",
+    "-setnumber",
+    "Mesh.Recombine3DAll",
+    "1",
+    "-setnumber",
+    "Mesh.SaveWithoutOrphans",
+    "1",
+    "-setnumber",
+    "Mesh.Smoothing",
+    "5",
+]
